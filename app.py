70ff8994096ffed04d577ad502a703ada3ee6f4d
miguelmachadoaa <miguelmachadoaa@gmail.com>
2025-03-09 07:23:25 -0400

    conectar con firebase

--- app.py
+++ app.py
@@ -1,53 +1,47 @@
 from flask import Flask, jsonify, request
 import mysql.connector 
+import firebase_admin 
+from firebase_admin import credentials, db
 
 app = Flask(__name__)
 
-# Configuración de la conexión a MySQL
-db_config = {
-    'host': '144.217.200.63',        # Cambia a tu servidor MySQL
-    'user': 'maymicov_agencia',       # Usuario de MySQL
-    'password': '2MkPvV6n',# Contraseña de MySQL
-    'database': 'maymicov_agenciamaymi' # Nombre de la base de datos
-}
+cred = credentials.Certificate("presupuesto-d827e-firebase-adminsdk-nwxyl-0ee0565833.json") # Cambia por tu archivo JSON
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://api-5b334-default-rtdb.firebaseio.com/' # Cambia por tu URL de Firebase
+})
 
 @app.route('/')
 def index():
-    return "API para MySQL"
+    params = request.args
+    id = params.get('id') 
+    itemId = params.get('itemId') 
+
+    if id:
+        if itemId:
+            ref = db.reference('{id}/{itemId}'.format(id=id, itemId=itemId))
+            datos = ref.get()
+            return jsonify(datos)
+        else:
+            #buscar todos los videos
+            ref = db.reference(id)
+            datos = ref.get()
+            return jsonify(datos)
+    else:
+        return jsonify({"mensaje": "No se han enviado datos"})
 
 
-# Ruta para obtener datos de MySQL
-@app.route('/data', methods=['GET'])
-def get_data():
-    try:
-        conn = mysql.connector.connect(**db_config)
-        cursor = conn.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM users")  # Ajusta según tu tabla
-        rows = cursor.fetchall()
-        return jsonify(rows)
-    except mysql.connector.Error as err:
-        return jsonify({"error": str(err)})
-    finally:
-        cursor.close()
-        conn.close()
+@app.route('/', methods=['POST'])
+def insertar():
+    params = request.args
+    id = params.get('id') 
 
-# Ruta para insertar datos en MySQL
-@app.route('/data', methods=['POST'])
-def insert_data():
-    try:
-        conn = mysql.connector.connect(**db_config)
-        cursor = conn.cursor()
-        data = request.json
-        query = "INSERT INTO users (columna1, columna2) VALUES (%s, %s)"
-        values = (data['columna1'], data['columna2'])
-        cursor.execute(query, values)
-        conn.commit()
-        return jsonify({"mensaje": "Datos insertados correctamente"})
-    except mysql.connector.Error as err:
-        return jsonify({"error": str(err)})
-    finally:
-        cursor.close()
-        conn.close()
+    if not id:
+        return jsonify({"mensaje": "No se ha enviado el ID"})
+    
+    ref = db.reference(id)
+    data = request.json
+    ref.push(data)
+    return jsonify({"mensaje": "Datos insertados correctamente"})
 
 if __name__ == '__main__':
     app.run(debug=True)
